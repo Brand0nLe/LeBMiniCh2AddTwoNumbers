0de0a990ae10aa9d59fd8d963561c6a192a72b16
Brand0nLe <93brandonle@gmail.com>
2022-10-18 22:59:30 -0700

    Loop Added

--- Program.cs
+++ Program.cs
@@ -2,14 +2,18 @@
 //10-18-22
 //Mini Challenge 2 - Add Two Numbers
 
-//need to add loop and data validation
+//need to add data validation
 string numberOne = "";
 string numberTwo = "";
 int numOne;
 int numTwo;
+bool playAgain = true;
+string userInput = "";
 
 
 
+while(playAgain == true)
+{
 Console.WriteLine("Let's do some math together!");
 Console.WriteLine("Enter your first number:");
 numberOne = Console.ReadLine();
@@ -22,3 +26,23 @@ numTwo = Convert.ToInt32(numberTwo);
 
 
 Console.WriteLine($"Your sum is: {numOne + numTwo}");
+Console.WriteLine("Would you like to play again?");
+Console.WriteLine("YES/NO");
+userInput = Console.ReadLine().ToUpper();
+
+if (userInput == "YES")
+{
+    playAgain = true;
+}
+
+else
+{
+    playAgain = false;
+    Console.WriteLine("Well it was fun while it lasted. See you next time!");
+}
+
+
+
+
+
+}

6d280532b40c0ca007acf56f93b9805ac161aee2
Brand0nLe <93brandonle@gmail.com>
2022-10-18 22:27:35 -0700

    First Commit

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿//Cuong Brandon Le
+//10-18-22
+//Mini Challenge 2 - Add Two Numbers
+
+//need to add loop and data validation
+string numberOne = "";
+string numberTwo = "";
+int numOne;
+int numTwo;
+
+
+
+Console.WriteLine("Let's do some math together!");
+Console.WriteLine("Enter your first number:");
+numberOne = Console.ReadLine();
+
+Console.WriteLine("What do you want for your second number?");
+numberTwo = Console.ReadLine();
+
+numOne = Convert.ToInt32(numberOne);
+numTwo = Convert.ToInt32(numberTwo);
+
+
+Console.WriteLine($"Your sum is: {numOne + numTwo}");

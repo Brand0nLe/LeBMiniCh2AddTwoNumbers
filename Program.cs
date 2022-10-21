4f548139fcf82b6f8737052a3cb34477a6c91356
Brand0nLe <93brandonle@gmail.com>
2022-10-20 19:10:50 -0700

    Final check and clean up of code

--- Program.cs
+++ Program.cs
@@ -4,6 +4,10 @@
 //Project takes 2 user inputs and stores them as integers. It then adds the two numbers and prints 
 //the sum into the terminal. Has a play again option with data validation if user doesn't enter YES. 
 //There's also data validation for when first two user inputs are not numbers.
+//Peer Review: Griffin Parker - This program works exactly as it should. It takes your numbers, validates them both seperately to make sure that they're both numbers, and then adds them together to give the user the sum. It lets the user choose to restart the program by typing "yes"; anything else will end the program. 
+//I think this program meets all the criteria of the assignment, but I would just recommend adding data validation on the restart section of the program to make sure the user has to type 'yes' or 'no' to loop and end the program respectively. I would also recommend adding a Console.Clear() at the beginning of the loop 
+//of the program to keep the terminal less cluttered.
+
 
 string numberOne = "";
 string numberTwo = "";
@@ -15,8 +19,12 @@ string userInput = "";
 
 
 
+
 while(playAgain == true)
 {
+    
+Console.Clear();
+
 Console.WriteLine("Let's do some math together!");
 Console.WriteLine("------------------------------");
 Console.WriteLine("Enter your first number:");
